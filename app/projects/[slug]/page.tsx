97df0532de84c9c9649bff94c028dfa0107a3b71
nkane312 <nkane312@gmail.com>
2024-12-30 17:01:31 -0600

    Fixes to github URL for projects

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -40,7 +40,7 @@ export default async function PostPage({ params }: Props) {
 						</div>
 						<div className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
 							<div className="grid grid-cols-1 gap-y-6 gap-x-8 text-base font-semibold leading-7 text-white sm:grid-cols-2 md:flex lg:gap-x-10">
-								<a target="_blank" href={`/projects/${project.repository}`}>
+								<a target="_blank" href={`https://github.com/${project.repository}`}>
 									GitHub <span aria-hidden="true">→</span>
 								</a>
 							</div>
