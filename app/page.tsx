2a3d71a29f7d3c5c0ea8429901851c64223657a7
nkane312 <nkane312@gmail.com>
2026-04-01 10:26:33 -0500

    minor copy and file updates

--- app/page.tsx
+++ app/page.tsx
@@ -40,7 +40,7 @@ export default function Home() {
 
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
-				<h2 className="text-base text-zinc-400 ">Front End Software Engineer.</h2>
+				<h2 className="text-base text-zinc-400 ">Software Engineer.</h2>
 			</div>
 		</main>
 	);
